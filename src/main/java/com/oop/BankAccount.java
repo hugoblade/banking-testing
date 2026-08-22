c86fbc7e3ff5e081f1e1b4abf2ab09150b1bf6d1
hugoblade <musondalimpomwenya>
2026-08-22 23:11:08 +0200

    Add JUnit dependency and first BankAccount test

--- src/main/java/com/oop/BankAccount.java
+++ src/main/java/com/oop/BankAccount.java
@@ -10,6 +10,6 @@ public class BankAccount {
             System.out.println("Insufficient funds");
             return;
         }
-        this.balance = this.balance - amount;
+        this.balance = this.balance * amount;
     }
 }
